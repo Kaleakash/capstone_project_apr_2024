dda598183eec4fe0ee054aa28fa1bba5cae8d6a4
akash <akash300383@gmail.com>
2024-04-21 21:10:18 +0530

    done

--- frontend-app/src/app/login.service.ts
+++ frontend-app/src/app/login.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs';
 
 @Injectable({providedIn:"root"})
 export class LoginService {
-
-  baseUrl:string ="http://localhost:9090/login"
+  //baseUrl:string ="http://localhost:9090/login"
+  baseUrl:string ="http://54.146.95.230:9090/login"
 
   constructor(public httpClient:HttpClient) { }  // DI for HttpClient
 
